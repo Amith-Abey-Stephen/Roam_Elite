cc2087f6fd9a8f4f467e2d3ceb02c18e810a6983
Amith-Abey-Stephen <amithabeystephen@gmail.com>
2024-05-12 21:38:31 +0530

    login hover

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const createBtn = document.getElementById('create');
 const loginBtn = document.getElementById('login');
 
 createBtn.addEventListener('mouseenter', function() {
-    loginBtn.style.backgroundColor = '#000';
+    loginBtn.style.backgroundColor = '#fff';
+    loginBtn.style.color = '#00224D'; 
 });
 
 createBtn.addEventListener('mouseleave', function() {
-    loginBtn.style.backgroundColor = 'aqua';
+    loginBtn.style.backgroundColor = '#00224D';
+    loginBtn.style.color = '#fff';
 });
